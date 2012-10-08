60731c236a11d19c50200c3f9661d15fea4a0406
alexander.fedorov <alexander.fedorov@rtlservice.com>
2012-10-08 14:16:15 +0000

    Fix sources.

--- cpu/native/net/udpclidev.h
+++ cpu/native/net/udpclidev.h
@@ -1,3 +1,6 @@
+#ifndef _UDP_DEV_H_
+#define _UDP_DEV_H_
+
 #include <stdint.h>
 
 #include "contiki-net.h"
@@ -5,3 +8,6 @@
 void udpclidev_init(void);
 uint16_t udpclidev_poll(void);
 uint8_t udpcli_send(uip_lladdr_t *lladdr);
+
+#endif /* _UDP_DEV_H_ */
+
